045c446d4339116086e7d0218db0f9bae4f35d15
omar sanchez <cosr2000@gmail.com>
2023-07-30 12:53:04 -0600

    proyecto terminado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -6,19 +6,19 @@ const Resultado = () => {
 
   //grados kelvin
   const kelvin = 273.15;
-  console.log(resultado);
+  
   return (
     <div className="contenedor clima">
-      <h2>El Clima de {name} es:</h2>
+      <h2> {name}:</h2>
       <p>
-        {parseInt(main.temp - kelvin)} <span>&#x2103;</span>{" "}
+        {parseInt(main.temp - kelvin)}<span>&#x2103;</span>{" "}
       </p>
       <div  className="temp_min_max">
         <p>
-          Min: {parseInt(main.temp_min - kelvin)} <span>&#x2103;</span>{" "}
+          Min: {parseInt(main.temp_min - kelvin)}<span>&#x2103;</span>{" "}
         </p>
         <p>
-          Max: {parseInt(main.temp_max - kelvin)} <span>&#x2103;</span>{" "}
+          Max: {parseInt(main.temp_max - kelvin)}<span>&#x2103;</span>{" "}
         </p>
       </div>
     </div>
